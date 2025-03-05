9ab96d4a09c23e04850219b1c3be48ec46c49fa6
Md Mosabbir Hossain <107846555+Mosabbir512@users.noreply.github.com>
2025-03-05 10:11:44 +0600

    Update LoginController.java

--- HallEats/HallEats/src/main/java/com/example/halleats/Controllers/LoginController.java
+++ HallEats/HallEats/src/main/java/com/example/halleats/Controllers/LoginController.java
@@ -100,26 +100,7 @@ public class LoginController {
 return  mv;
     }
 
-    @GetMapping("register")
-
-    public  ModelAndView register(HttpServletRequest request)
-    {
-        ModelAndView mv= new ModelAndView();
-        HttpSession session = request.getSession(false); // Check if a session already exists
-
-        if (session != null && session.getAttribute("user") != null) {
-            // User is already logged in, redirect to the home page
-            mv.setViewName("Home");
-            mv.addObject("msg", token.getName());
-        }
-        else
-        {
-            mv.setViewName("register");
-        }
-        return mv;
-
-
-    }
+    
     @RequestMapping ("checkregister")
     public ModelAndView checkregister(Register register,HttpServletRequest request)
     {
