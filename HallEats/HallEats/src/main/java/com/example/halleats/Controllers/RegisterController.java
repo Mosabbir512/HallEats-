1a32233a479bfdcb4e6fd6a8cef847c93119a34d
Md Mosabbir Hossain <107846555+Mosabbir512@users.noreply.github.com>
2025-03-05 10:12:00 +0600

    Update RegisterController.java

--- HallEats/HallEats/src/main/java/com/example/halleats/Controllers/RegisterController.java
+++ HallEats/HallEats/src/main/java/com/example/halleats/Controllers/RegisterController.java
@@ -4,6 +4,25 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class RegisterController {
+@GetMapping("register")
 
+    public  ModelAndView register(HttpServletRequest request)
+    {
+        ModelAndView mv= new ModelAndView();
+        HttpSession session = request.getSession(false); // Check if a session already exists
+
+        if (session != null && session.getAttribute("user") != null) {
+            // User is already logged in, redirect to the home page
+            mv.setViewName("Home");
+            mv.addObject("msg", token.getName());
+        }
+        else
+        {
+            mv.setViewName("register");
+        }
+        return mv;
+
+
+    }
 
 }
